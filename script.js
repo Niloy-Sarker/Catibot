91389c28af0d1a0b4c6e385f287779863b7e5ad8
Niloy Sarker <69155993+Niloy-Sarker@users.noreply.github.com>
2024-09-11 22:40:50 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+const _0x3d007d = "font-weight: bold; color: red;";
+const _0x54054d = "font-weight: bold; color: green;";
+const _0x4f2050 = "%c [AutoBot] ";
+console.clear();
+console.log(`${_0x4f2050} inserting... `, _0x54054d);
+
+try {
+  function _0x36c22e() {
+    P.cat.isAuto = !P.cat.isAuto;
+    if (P.cat.isAuto) {
+      this.ani8.play(0, true);
+      Laya.timer.loop(500, this, this.checkAuto);
+      this.checkFreeCat();
+      this.m_btn_AutoSetting.visible = true;
+    } else {
+      Laya.timer.clearAll(this.checkAuto);
+      this.ani8.stop();
+      Laya.timer.loop(5000, this, this.checkSum);
+      this.m_btn_AutoSetting.visible = false;
+    }
+    this.m_img_StopAuto.visible = !P.cat.isAuto;
+    u(
+      `AutoBot ${
+        P.cat.isAuto ? " activated " : " turned off"
+      }!\n\n Cracked by @BartaNawOfficial `
+    );
+  }
+
+  // Assign the _0x36c22e function to N's prototype instead
+  N.prototype.onClickAuto = _0x36c22e;
+  console.log(`${_0x4f2050} Script loaded `, _0x54054d);
+  console.log(`${_0x4f2050} Crack by BartaNawOfficial `, _0x54054d);
+} catch (e) {
+  console.log(
+    `${_0x4f2050}An error occurred, the breakpoint was not set correctly!`,
+    _0x3d007d
+  );
+  console.log(
+    `${_0x4f2050}Please follow the instructions, and you will succeed :*`,
+    _0x3d007d
+  );
+}
+
+// Displaying the decoded information (optional)
+function _0x2f46(_0x1c3d5e) {
+  const _0x139711 = atob(_0x1c3d5e);
+  return decodeURIComponent(escape(_0x139711));
+}
+
+const _0x30c40a = "ICBBdXRob3IgQ2hhbm5lbDog aHR0cHM6Ly90Lm1lL0JhcnRh TmF3T2ZmaWNpYWwg"; 
+const _0x5a57 = "IEF1dGhvciBMaW5rOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20vQXNIaWs3MDU=";
+
+const _0x55f3d6 = _0x2f46(_0x30c40a);
+const _0x334ae3 = _0x2f46(_0x5a57);
+console.log(_0x55f3d6);
+console.log(_0x334ae3);
